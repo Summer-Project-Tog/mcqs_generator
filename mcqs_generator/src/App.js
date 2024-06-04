540801db15d7b5622403bed614e33a6b44f52fb4
eskimofishes <eskimofises@gmail.com>
2024-06-05 00:06:20 +0800

    Michael! Emily !

--- mcqs_generator/src/App.js
+++ mcqs_generator/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!!! Junyu!!
+          Hi there!!! Junyu!! Michael!! Emily !!
         </p>
         <a
           className="App-link"
