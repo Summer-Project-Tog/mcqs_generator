e0cb30b256558acd6bea9eae44d8c0433ab8795b
Michael Santoso <michaelsantoso40@gmail.com>
2024-06-05 00:30:52 +0800

    hi got changes

--- mcqs_generator/src/App.js
+++ mcqs_generator/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!!! Junyu!! Michael!! Emily !!
+          Hi there!!! Junyu!! Michael!!
+          There is more messages here!!!
         </p>
         <a
           className="App-link"
