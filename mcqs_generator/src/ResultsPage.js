e754e88e900f67a1626c6949c7271e7d97a6a6ab
Michael Santoso <michaelsantoso40@gmail.com>
2024-06-07 01:08:07 +0800

    fix css button in results page

--- mcqs_generator/src/ResultsPage.js
+++ mcqs_generator/src/ResultsPage.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import facebookLogo from './assets/images/facebook_logo.png';
-import twitterLogo from './assets/images/twitter_logo.png';
-import instagramLogo from './assets/images/instagram_logo.png';
-import './ResultsPage.css';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import facebookLogo from "./assets/images/facebook_logo.png";
+import twitterLogo from "./assets/images/twitter_logo.png";
+import instagramLogo from "./assets/images/instagram_logo.png";
+import "./ResultsPage.css";
 import { db, collection, getDocs } from "./firebase.js";
 
 function ResultsPage() {
@@ -18,17 +18,22 @@ function ResultsPage() {
       const querySnapshot = await getDocs(collection(db, "Answers"));
       const correctAnswersObj = {};
       querySnapshot.forEach((doc) => {
-        console.log(`Fetched document: ${doc.id}, data: ${JSON.stringify(doc.data())}`);
+        console.log(
+          `Fetched document: ${doc.id}, data: ${JSON.stringify(doc.data())}`
+        );
         correctAnswersObj[doc.id] = doc.data().answer;
       });
       setCorrectAnswers(correctAnswersObj);
 
-      console.log('Fetched Correct Answers:', correctAnswersObj);
+      console.log("Fetched Correct Answers:", correctAnswersObj);
 
       let correctCount = 0;
       Object.keys(answers).forEach((questionId) => {
         console.log(`User Answer for ${questionId}:`, answers[questionId]);
-        console.log(`Correct Answer for ${questionId}:`, correctAnswersObj[questionId]);
+        console.log(
+          `Correct Answer for ${questionId}:`,
+          correctAnswersObj[questionId]
+        );
 
         if (answers[questionId] === correctAnswersObj[questionId]) {
           correctCount++;
@@ -43,11 +48,11 @@ function ResultsPage() {
   const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
-    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
   };
 
   const handleShowAnswers = () => {
-    navigate('/mcq', { state: { answers, correctAnswers } });
+    navigate("/mcq", { state: { answers, correctAnswers } });
   };
 
   return (
@@ -62,20 +67,54 @@ function ResultsPage() {
           </nav>
         </div>
         <div className="content">
-          <h1>{score / Object.keys(correctAnswers).length > 0.5 ? 'Congratulations!' : 'Try harder next time!'}</h1>
-          <p>Your results: {score}/{Object.keys(correctAnswers).length} ({((score / Object.keys(correctAnswers).length) * 100).toFixed(2)}%)</p>
-          <p>Status: {score / Object.keys(correctAnswers).length > 0.5 ? 'PASSED' : 'FAILED'}</p>
+          <h1>
+            {score / Object.keys(correctAnswers).length > 0.5
+              ? "Congratulations!"
+              : "Try harder next time!"}
+          </h1>
+          <p>
+            Your results: {score}/{Object.keys(correctAnswers).length} (
+            {((score / Object.keys(correctAnswers).length) * 100).toFixed(2)}%)
+          </p>
+          <p>
+            Status:{" "}
+            {score / Object.keys(correctAnswers).length > 0.5
+              ? "PASSED"
+              : "FAILED"}
+          </p>
           <p>Time: {formatTime(timer)}</p>
-          <button onClick={handleShowAnswers}>Show Answers</button>
-          <Link to="/">Go back to Home</Link>
+          <button onClick={handleShowAnswers} className="handle-answers-button">
+            Show Answers
+          </button>
+          <Link to="/">
+            <button className="home-button"> Go back to Home</button>
+          </Link>
         </div>
       </header>
       <footer className="App-footer">
         <p>MCQ Gen</p>
         <div className="social-icons">
-          <a href="#facebook"><img src={facebookLogo} alt="Facebook" className="social-media-logo" /></a>
-          <a href="#twitter"><img src={twitterLogo} alt="Twitter" className="social-media-logo" /></a>
-          <a href="#instagram"><img src={instagramLogo} alt="Instagram" className="social-media-logo" /></a>
+          <a href="#facebook">
+            <img
+              src={facebookLogo}
+              alt="Facebook"
+              className="social-media-logo"
+            />
+          </a>
+          <a href="#twitter">
+            <img
+              src={twitterLogo}
+              alt="Twitter"
+              className="social-media-logo"
+            />
+          </a>
+          <a href="#instagram">
+            <img
+              src={instagramLogo}
+              alt="Instagram"
+              className="social-media-logo"
+            />
+          </a>
         </div>
       </footer>
     </div>
